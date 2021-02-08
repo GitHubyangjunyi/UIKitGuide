732bca5bdc60ff262420d0d167b9e8aabf82fb7e
杨俊艺 <32793979+GitHubyangjunyi@users.noreply.github.com>
2021-02-08 14:42:32 +0800

    添加Xib工厂

--- XibFactory/XibFactory/ViewController.swift
+++ XibFactory/XibFactory/ViewController.swift
@@ -11,9 +11,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let normal = ToolBar.createToolBar(withType: .normal)
+        normal?.frame.origin = .init(x: 0, y: 100)
+        
+        let edit = ToolBar.createToolBar(withType: .edit)
+        edit?.frame.origin = .init(x: 0, y: 200)
+        
+        view.addSubview(normal!)
+        view.addSubview(edit!)
+        
     }
 
 
 }
 
+class ToolBar: UIView {
+    enum ToolBarType {
+        case normal
+        case edit
+    }
+    
+    class func createToolBar(withType type: ToolBarType) -> ToolBar? {
+        switch type {
+        case .normal:
+            return Bundle.main.loadNibNamed("ToolBar", owner: nil, options: nil)?[0] as? ToolBar
+        case .edit:
+            return Bundle.main.loadNibNamed("ToolBar", owner: nil, options: nil)?[1] as? ToolBar
+        }
+    }
+
+    func handleEvent() {
+        // 子类重写方法进行自定义
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        handleEvent()
+    }
+    
+}
+
+class ToolBarNormal: ToolBar {
+    override func handleEvent() {
+        backgroundColor = .gray
+    }
+}
+
+class ToolBarEdit: ToolBar {
+    override func handleEvent() {
+        backgroundColor = .darkGray
+    }
+}
+
+// 创建Empty模版的ToolBar.xib
+// ToolBar.xib分别添加两个UIView并设置各自的Custom Class为ToolBarNormal和ToolBarEdit
