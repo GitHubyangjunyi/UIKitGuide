7311cf63baf3d83f8ca439d6a496fe19319c99e6
杨俊艺 <32793979+GitHubyangjunyi@users.noreply.github.com>
2021-02-08 11:18:47 +0800

    初始化

---
+++ XibFactory/XibFactory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XibFactory
+//
+//  Created by 杨俊艺 on 2021/2/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
